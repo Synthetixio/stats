2e83362fea7fe29a3f67df54227e8462b2de99c9
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-09-18 09:36:08 +1000

    fix: Synths folder casing

---
+++ sections/Synths/SynthsBarChart.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+
+import SidewaysBarChart from 'components/Charts/SidewaysBarChart';
+import { ChartTitle, ChartSubtitle } from 'components/common';
+import { OpenInterest } from 'types/data';
+
+type SynthsBarChartProps = {
+	data: OpenInterest;
+};
+
+const SynthsBarChart: FC<SynthsBarChartProps> = ({ data }) => (
+	<SynthsBarChartContainer>
+		<ChartTitle>SYNTH vs iSYNTH</ChartTitle>
+		<ChartSubtitle>Long/short interest on crptoassets</ChartSubtitle>
+		<SidewaysBarChart data={data} />
+	</SynthsBarChartContainer>
+);
+
+const SynthsBarChartContainer = styled.div`
+	background: ${(props) => props.theme.colors.mediumBlue};
+	width: 49%;
+	height: 680px;
+	overflow-y: scroll;
+	@media only screen and (max-width: 854px) {
+		width: 100%;
+	}
+`;
+
+export default SynthsBarChart;
---
+++ sections/Synths/SynthsPieChart.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+
+import PieChart from 'components/Charts/PieChart';
+import { ChartTitle, ChartSubtitle } from 'components/common';
+import { SynthTotalSupply } from 'types/data';
+
+type SynthsPieChartProps = {
+	data: SynthTotalSupply[];
+};
+
+const SynthsPieChart: FC<SynthsPieChartProps> = ({ data }) => (
+	<SynthsPieChartContainer>
+		<ChartTitle>SYNTH DOMINANCE</ChartTitle>
+		<ChartSubtitle>Distribution of synths within the network</ChartSubtitle>
+		<PieChart data={data} />
+	</SynthsPieChartContainer>
+);
+
+const SynthsPieChartContainer = styled.div`
+	background: ${(props) => props.theme.colors.mediumBlue};
+	width: 49%;
+	height: 680px;
+	@media only screen and (max-width: 854px) {
+		width: 100%;
+	}
+`;
+
+export default SynthsPieChart;
