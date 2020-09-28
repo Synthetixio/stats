e87744de37325ef8ce99de4663cd82883cfbd369
David Schwartz <dbschwartz17@gmail.com>
2020-09-28 11:16:00 -0700

    Update keys (#21)
    Co-authored-by: Clément BALESTRAT <clement.balestrat@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,12 @@ export const headersAndScrollRef: { [key: string]: RefObject<unknown> } = {
 	OPTIONS: createRef(),
 };
 
-const provider = new ethers.providers.InfuraProvider('homestead', process.env.INFURA_KEY);
+console.log('process.env.NEXT_PUBLIC_INFURA_KEY', process.env.NEXT_PUBLIC_INFURA_KEY);
+
+const provider = new ethers.providers.InfuraProvider(
+	'homestead',
+	process.env.NEXT_PUBLIC_INFURA_KEY
+);
 export const ProviderContext = createContext(provider);
 
 export const HeadersContext = createContext(headersAndScrollRef);
