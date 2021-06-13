552895ccc1bd57ddf1a6ae10a9d4c138d6b4ddbf
Daniel Beal <git@danb.email>
2021-06-12 23:15:18 -0700

    significant improvements for liquidation information
    * improve speed of loading liquidation table info by a huge margin
    * fix column sorting to work perfectly for all columns
    * add liquidation totals
    
    /hours 4

--- queries/staking/useLiquidationsQuery.ts
+++ queries/staking/useLiquidationsQuery.ts
@@ -1,42 +1,98 @@
 import { useContext } from 'react';
 import { useQuery } from 'react-query';
-import { BigNumber } from 'ethers';
+import { BigNumber, ethers } from 'ethers';
 import { SNXJSContext } from 'pages/_app';
 import snxData from 'synthetix-data';
 
 import QUERY_KEYS from 'constants/queryKeys';
+import { useSNXInfo } from 'queries/shared/useSNXInfo';
+import _ from 'lodash';
+
+export type LiquidationsSummary = {
+	amountToCover: number;
+	totalLiquidatableSNX: number;
+	liquidatableCount: number;
+};
 
 export type LiquidationsData = {
 	deadline: number;
 	account: string;
 	currentRatio: number;
 	currentCollateral: number;
-	currentBalanceOf: number;
+	currentDebt: number;
+	amountToCover: number;
+	liquidatableAmount: number;
 };
 
 export const useLiquidationsQuery = () => {
 	const snxjs = useContext(SNXJSContext);
-	return useQuery<LiquidationsData[], string>(QUERY_KEYS.Staking.Liquidations, async () => {
-		const activeLiquidations = await snxData.liquidations.getActiveLiquidations();
-		const liquidations = [];
-		for (let i = 0; i < activeLiquidations.length; i++) {
-			let promises = await Promise.all([
-				snxjs.contracts.Synthetix.collateralisationRatio(activeLiquidations[i].account),
-				snxjs.contracts.Synthetix.collateral(activeLiquidations[i].account),
-				snxjs.contracts.Synthetix.balanceOf(activeLiquidations[i].account),
-			]);
 
-			let [currentRatio, currentCollateral, currentBalanceOf] = promises.map((val: BigNumber) =>
-				Number(snxjs.utils.formatEther(val))
-			);
-			liquidations.push({
-				deadline: activeLiquidations[i].deadline,
-				account: activeLiquidations[i].account,
-				currentRatio,
-				currentCollateral,
-				currentBalanceOf,
+	const { SNXPrice, issuanceRatio, totalIssuedSynths, lastDebtLedgerEntry } = useSNXInfo(snxjs);
+
+	return useQuery<[LiquidationsSummary, LiquidationsData[]], string>(
+		QUERY_KEYS.Staking.Liquidations,
+		async () => {
+			const activeLiquidations = await snxData.liquidations.getActiveLiquidations();
+
+			const accountAddresses = activeLiquidations.map((l: any) => l.account.toLowerCase());
+
+			const rawAccountInfos = await snxData.pageResults({
+				api: snxData.graphAPIEndpoints.snx,
+				query: {
+					entity: 'snxholders',
+					selection: {
+						where: {
+							id_in: '[' + accountAddresses.map((v: string) => `\\"${v}\\"`).join(',') + ']',
+						},
+					},
+					properties: ['id', 'collateral', 'initialDebtOwnership', 'debtEntryAtIndex'],
+				},
 			});
-		}
-		return liquidations;
-	});
+
+			const accountInfos = _.keyBy(rawAccountInfos, 'id');
+
+			const summary: LiquidationsSummary = {
+				amountToCover: 0,
+				totalLiquidatableSNX: 0,
+				liquidatableCount: activeLiquidations.length,
+			};
+
+			const liquidations = [];
+			for (const l of activeLiquidations) {
+				const accountInfo = accountInfos[l.account.toLowerCase()];
+				const debtEntryAtIndexFmt = Number(
+					ethers.utils.formatEther(ethers.BigNumber.from(accountInfo.debtEntryAtIndex))
+				);
+				const initialDebtOwnershipFmt = Number(
+					ethers.utils.formatEther(ethers.BigNumber.from(accountInfo.initialDebtOwnership))
+				);
+
+				let currentDebt =
+					((totalIssuedSynths! * lastDebtLedgerEntry!) / debtEntryAtIndexFmt) *
+					initialDebtOwnershipFmt;
+
+				let currentCollateral = Number(
+					ethers.utils.formatEther(ethers.BigNumber.from(accountInfo.collateral))
+				);
+
+				const amountToCover = currentDebt - issuanceRatio! * currentCollateral * SNXPrice!;
+				const liquidatableAmount = amountToCover / SNXPrice!;
+
+				liquidations.push({
+					deadline: l.deadline,
+					account: l.account,
+					currentRatio: currentDebt / (currentCollateral * SNXPrice!),
+					currentCollateral,
+					currentDebt,
+					amountToCover,
+					liquidatableAmount,
+				});
+
+				summary.amountToCover += amountToCover;
+				summary.totalLiquidatableSNX += liquidatableAmount;
+			}
+			return [summary, liquidations];
+		},
+		{ enabled: !!SNXPrice && !!issuanceRatio && !!totalIssuedSynths && !!lastDebtLedgerEntry }
+	);
 };
