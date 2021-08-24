b37eeaff71c28cc678e7811f514eeaf9387e7609
dbeal <git@dbeal.dev>
2021-08-24 15:52:56 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -185,7 +185,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
 				  (function() {
 				    var u="https://analytics.synthetix.io/";
 				    _paq.push(['setTrackerUrl', u+'matomo.php']);
-				    _paq.push(['setSiteId', '2']);
+				    _paq.push(['setSiteId', '4']);
 				    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 				    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 				  })();
